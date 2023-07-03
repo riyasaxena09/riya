9222b78f68fac0e94bc544375fd6b9c7549cd717
riyasaxena09 <riyasaxenabdn2002@gmail.com>
2023-07-03 08:11:08 +0530

    initial commit

---
+++ ExpensseItem.js
@@ -0,0 +1,12 @@
+
+function ex(){
+    return( 
+        <div>
+    <h1>Expense Item!!</h1>
+    <div>food rs 100</div>
+    <div>petrol rs 200</div>
+    <div>movie rs 1000</div>
+    </div>
+    );
+}
+export default ex;
