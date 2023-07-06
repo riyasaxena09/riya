27588b84bd93b7441e4876d3033bd75b1246552f
riyasaxena09 <riyasaxenabdn2002@gmail.com>
2023-07-06 10:44:45 +0530

    initial commit

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,17 +1,27 @@
+import React,{ useState } from 'react';
 import './Expense.css';
 import Card from './card';
 import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props){
+    const [title,sett]=useState(props.title);
+    const [amount,seta]=useState(props.amount);
+ function clickexpense(){
+    seta('100');
+    console.log(amount);
+ }
 function clickHandler(){
-    console.log("clikc");
+    
+    sett('update');
+    console.log(title);
+    clickexpense();
  } 
     return( 
         <Card className='expense-item'>
   <ExpenseDate date={props.date}></ExpenseDate>
    <div className='expense-item__description'>
-    <div className='expense-item h2'>{props.title}</div>
-<div className='expense-item__price'>${props.amount}</div>
+    <div className='expense-item h2'>{title}</div>
+<div className='expense-item__price'>${amount}</div>
 
    </div>
    <button onClick={clickHandler}>Delete Expense</button>
